ce7f3dc4206f3abb4014ac942a1d3c5f4d925f7d
Yu_Jehyeop <fbwpguq@gmail.com>
2021-10-14 23:52:02 +0900

    미리보기 이미지 클릭을 통한 선택 취소, 이미지 리스트로 db에 저장

--- static/js/photo.js
+++ static/js/photo.js
@@ -5,20 +5,33 @@ $(document).ready(function () {
     $("#file").on("change", handleImgsFilesSelect);
 })
 
+
 function upload_menue(){
     $('#place-list').hide();
     $('#photo-place-test').show();
 }
+
 
 function clickPlace(lat, lng){
     $('#click-place-lat').val(lat);
     $('#click-place-lng').val(lng);
 }
 
+
+function fileUploadAction(){
+    console.log("fileUploadAction");
+    $("#file").trigger('click');
+}
+
+
 function handleImgsFilesSelect(e){
+    sel_files = []
+    $(".images").empty()
+
     let files = e.target.files;
     let filesArr = Array.prototype.slice.call(files);
 
+    let index = 0;
     filesArr.forEach(function (f){
         if(!f.type.match("image.*")) {
             alert("확장자는 이미지 확장자만 가능합니다.")
@@ -28,23 +41,39 @@ function handleImgsFilesSelect(e){
 
         let reader = new FileReader();
         reader.onload = function(e){
-            let img_html = "<span class=\"image-card\"><img src=\""+e.target.result + "\"/></span>";
-            $(".images").append(img_html);
+            let html = "<a href=\"javascript:void(0);\" onclick=\"deleteImageAction("+index+")\" id=\"img_id_"+index+"\"><span class=\"image-card\"><img src=\""+e.target.result + "\" data-file='"+f.name+"' class='sleProductFile' title='Click to remove'></span>";
+            $(".images").append(html);
+            index++;
+
+            // let img_html = "<span class=\"image-card\"><img src=\""+e.target.result + "\"/></span>";
+            // $(".images").append(img_html);
         }
         reader.readAsDataURL(f);
     })
 }
 
-function uploadphoto(){
+function deleteImageAction(index){
+    sel_files.splice(index,1);
+    let img_id = "#img_id_"+ index;
+    $(img_id).remove();
+
+}
+function addFiles() {
+            $("#upload").append("<input type='file' name='file'>");
+        }
+
+function uploadphoto() {
+
+    let form_data = new FormData()
+
     let lat = $('#click-place-lat').val();
     let lng = $('#click-place-lng').val();
 
-    let file = $('#file')[0].files[0]
-    let form_data = new FormData()
-
-    form_data.append("file_give", file)
-    form_data.append("lat_give", lat)
-    form_data.append("lng_give", lng)
+    for (let i=0; i<sel_files.length; i++) {
+        form_data.append("file_give", sel_files[i])
+    }
+        form_data.append("lat_give", lat)
+        form_data.append("lng_give", lng)
 
     $.ajax({
         type: "POST",
@@ -60,3 +89,15 @@ function uploadphoto(){
     });
 }
 
+function  submitAction(){
+    let data = new FormData();
+
+    for (let i=0,len=sel_files.length; i<len; i++){
+        let name = "image_"+i;
+        data.append(name,sel_files[i]);
+    }
+    data.append("image_count", sel_files.length);
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST","./")
+}
